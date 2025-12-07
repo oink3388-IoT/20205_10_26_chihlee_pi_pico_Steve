e98c82b4d81e837e0a4b6973bda7340afef1eba1
stevewang <oink3388@gmail.com>
2025-12-07 13:03:26 +0800

    modify

--- lesson5/lesson5.3_1.py
+++
@@ -1,9 +0,0 @@
-# 傳統版（同樣 100% 一定亮）
-from machine import Pin
-import time
-
-led = Pin("LED", Pin.OUT)
-
-while True:
-    led.toggle()
-    time.sleep(0.5)
--- lesson5/lesson5_1.py
+++
@@ -1,3 +0,0 @@
-from machine import Timer
-
-timer = Timer(period=2000, mode=Timer.PERIODIC, callback=lambda t:print('Hello! Pico'))
